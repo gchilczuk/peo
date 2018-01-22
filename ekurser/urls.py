4c723b38c35e787ac2ca046f6957a779ae2858e3
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-01-22 11:07:11 +0100

    path test

--- ekurser/urls.py
+++ ekurser/urls.py
@@ -4,12 +4,12 @@ from ekurser.kontroler.pracownik import TeacherHomeView, PetitionListView, Petit
 from ekurser.kontroler.views import main_view, clear_db
 
 urlpatterns = [
-    path('', main_view, name='main'),
-    path('student/', StudentHomeView.as_view(), name='student_home'),
-    path('student/grupy/', GroupPickerView.as_view(), name='group_picker'),
+    path('/', main_view, name='main'),
+    path('student', StudentHomeView.as_view(), name='student_home'),
+    path('student/grupy', GroupPickerView.as_view(), name='group_picker'),
     path('pracownik/', TeacherHomeView.as_view(), name='teacher_home'),
     path('pracownik/wnioski', PetitionListView.as_view(), name='petition_list'),
     path('pracownik/wnioski/<nr>', PetitionView.as_view(), name='petition'),
-    path('clear/', clear_db, name='clear' )
+    path('clear', clear_db, name='clear' )
 ]
 
