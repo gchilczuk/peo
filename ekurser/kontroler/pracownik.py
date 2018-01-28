dd5163ee45249d245446c6bbb5f0bb8fcb4d7574
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-01-28 03:21:52 +0100

    hm

--- ekurser/kontroler/pracownik.py
+++ ekurser/kontroler/pracownik.py
@@ -1,3 +1,5 @@
+"""Kontroler dla widoków pracownika"""
+
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
@@ -9,20 +11,26 @@ from .forms import petitionJudgeForm
 
 
 class TeacherHomeView(View):
+    """Widok domowy pracownika"""
+
+    user = NauczycielAkademicki.objects.get(id=1)
+
     def get(self, request):
-        user = NauczycielAkademicki.objects.get(id=1)
         location = [Location('Home', reverse('teacher_home'))]
-        context = {'navigation': Navigation(str(user), location, uid=user.id)}
+        context = {'navigation': Navigation(str(self.user), location, uid=self.user.id)}
 
         return render(request, 'wnioski/tHome.html', context)
 
 
 class PetitionListView(View):
+    """Widok listy wniosków do rozpatrzenia"""
+
+    user = NauczycielAkademicki.objects.get(id=1)
+
+
     def get(self, request):
-        user = NauczycielAkademicki.objects.get(id=1)
         location = [Location('Home', reverse('teacher_home')),
                     Location('wnioski o uruchomienie grupy', reverse('petition_list'))]
-
         wnioski = []
         for wniosek in WniosekOUruchomienieGrupyZajeciowej.objects.filter(zgodapelnomocnika=None):
             wnioski.append(
@@ -40,83 +48,76 @@ class PetitionListView(View):
                     id=wniosek.id
                 )
             )
-        context = {'navigation': Navigation(str(user), location, uid=user.id),
+        context = {'navigation': Navigation(str(self.user), location, uid=self.user.id),
                    'wnioski': wnioski}
         return render(request, 'wnioski/wnioskiBrowser.html', context)
 
 
 class PetitionView(View):
+    """Widok rozpatrywania wniosku"""
+
+    user = NauczycielAkademicki.objects.get(id=1)
+
+    wniosek = None
+
     def get(self, request, nr):
-        user = NauczycielAkademicki.objects.get(id=1)
         location = [Location('Home', reverse('teacher_home')),
                     Location('wnioski o uruchomienie grupy', reverse('petition_list')),
                     Location(nr, reverse('petition', args=[nr]))]
+        self.wniosek = WniosekOUruchomienieGrupyZajeciowej.objects.get(id=nr)
 
-        wniosek = WniosekOUruchomienieGrupyZajeciowej.objects.get(id=nr)
-        zgoda = 'Brak decyzji'
-        if wniosek.zgodapelnomocnika:
-            zgoda = 'Tak'
-        elif wniosek.zgodapelnomocnika is not None:
-            zgoda = 'Nie'
-        wniosek = Wniosek(numer=f'{wniosek.nrwniosku//10000}-{wniosek.nrwniosku%10000}',
-                          status=wniosek.statuswniosku.nazwa,
-                          kurs=wniosek.grupazajeciowa.kurs.nazwakursu,
-                          skladajacy=str(wniosek.student),
-                          prowadzacy=', '.join(
-                              [str(prow) for prow in wniosek.grupazajeciowa.prowadzacy.all()]) or "<brak>",
-                          zapisanych=wniosek.grupazajeciowa.liczbauczestnikow,
-                          rodzaj=wniosek.grupazajeciowa.rodzajgrupy.nazwa,
-                          sala=wniosek.grupazajeciowa.termin.sala,
-                          termin=f'{wniosek.grupazajeciowa.termin.dzien.nazwa } {wniosek.grupazajeciowa.termin.godzina } ' if wniosek.grupazajeciowa.termin.dzien else '<brak>',
-                          opinia='<brak>',
-                          zgoda=zgoda,
-                          id=wniosek.id)
-        context = {'navigation': Navigation(str(user), location, uid=user.id),
-                   'wniosek': wniosek}
+        context = {'navigation': Navigation(str(self.user), location, uid=self.user.id),
+                   'wniosek': self.create_wniosek_dto()}
         return render(request, 'wnioski/wniosek.html', context)
 
     def post(self, request, nr):
         postback = Postback()
-        user = NauczycielAkademicki.objects.get(id=1)
         location = [Location('Home', reverse('teacher_home')),
                     Location('wnioski o uruchomienie grupy', reverse('petition_list')),
                     Location(nr, reverse('petition', args=[nr]))]
-
-        wniosek = WniosekOUruchomienieGrupyZajeciowej.objects.get(id=nr)
+        self.wniosek = WniosekOUruchomienieGrupyZajeciowej.objects.get(id=nr)
         form = petitionJudgeForm(request.POST)
+
         if form.is_valid():
-            kurs_id = form.cleaned_data['kurs']
-            wniosek.zgodapelnomocnika = bool(int(form.cleaned_data['zgoda']))
-            wniosek.save()
-            postback.message = 'Wniosek został '
-            if wniosek.zgodapelnomocnika:
-                postback.message += 'zatwierdzony'
+            self.wniosek.rozpatrz(bool(int(form.cleaned_data['zgoda'])))
+
+            if self.wniosek.zgodapelnomocnika:
+                postback.message = 'Wniosek został zatwierdzony'
             else:
-                postback.message += 'odrzucony'
+                postback.message = 'Wniosek został odrzucony'
                 postback.success = False
         else:
             postback.success = False
             postback.message = "Coś poszło nie tak"
 
-        zgoda = 'Brak decyzji'
-        if wniosek.zgodapelnomocnika:
+        context = {'navigation': Navigation(str(self.user), location, uid=self.user.id),
+                   'wniosek': self.create_wniosek_dto(), 'postback': postback}
+        return render(request, 'wnioski/wniosek.html', context)
+
+    def create_wniosek_dto(self):
+        """Twordzy obiekt DTO na podstawie wniosku"""
+        if self.wniosek.zgodapelnomocnika is None:
+            zgoda = 'Brak decyzji'
+        elif self.wniosek.zgodapelnomocnika:
             zgoda = 'Tak'
-        elif wniosek.zgodapelnomocnika is not None:
+        else:
             zgoda = 'Nie'
 
-        wniosek_dto = Wniosek(numer=f'{wniosek.nrwniosku//10000}-{wniosek.nrwniosku%10000}',
-                              status=wniosek.statuswniosku.nazwa,
-                              kurs=wniosek.grupazajeciowa.kurs.nazwakursu,
-                              skladajacy=str(wniosek.student),
-                              prowadzacy=', '.join(
-                                  [str(prow) for prow in wniosek.grupazajeciowa.prowadzacy.all()]) or "<brak>",
-                              zapisanych=wniosek.grupazajeciowa.liczbauczestnikow,
-                              rodzaj=wniosek.grupazajeciowa.rodzajgrupy.nazwa,
-                              sala=wniosek.grupazajeciowa.termin.sala,
-                              termin=f'{wniosek.grupazajeciowa.termin.dzien.nazwa } {wniosek.grupazajeciowa.termin.godzina } ' if wniosek.grupazajeciowa.termin.dzien else '<brak>',
-                              opinia='<brak>',
-                              zgoda=zgoda,
-                              id=wniosek.id)
-        context = {'navigation': Navigation(str(user), location, uid=user.id),
-                   'wniosek': wniosek_dto, 'postback': postback}
-        return render(request, 'wnioski/wniosek.html', context)
+        if self.wniosek.grupazajeciowa.termin.dzien:
+            termin = f'{self.wniosek.grupazajeciowa.termin.dzien.nazwa } {self.wniosek.grupazajeciowa.termin.godzina } '
+        else:
+            termin = '<brak>',
+
+        return Wniosek(numer=f'{self.wniosek.nrwniosku//10000}-{self.wniosek.nrwniosku%10000}',
+                       status=self.wniosek.statuswniosku.nazwa,
+                       kurs=self.wniosek.grupazajeciowa.kurs.nazwakursu,
+                       skladajacy=str(self.wniosek.student),
+                       prowadzacy=', '.join(
+                           [str(prow) for prow in self.wniosek.grupazajeciowa.prowadzacy.all()]) or "<brak>",
+                       zapisanych=self.wniosek.grupazajeciowa.liczbauczestnikow,
+                       rodzaj=self.wniosek.grupazajeciowa.rodzajgrupy.nazwa,
+                       sala=self.wniosek.grupazajeciowa.termin.sala,
+                       termin=termin,
+                       opinia='<brak>',
+                       zgoda=zgoda,
+                       id=self.wniosek.id)
